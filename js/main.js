b79d0755f58d8f8c85ece5abf954159ec296361d
Zophie Tucker <zophietucker@gmail.com>
2025-12-10 21:29:16 +1100

    Update API endpoint for fetch request

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ import { initKeyboardShortcuts } from './keyboardShortcuts.js';
     const timeoutId = setTimeout(() => controller.abort(), 4000);
     try {
       Telemetry.log('share:shorten:start', { urlLength: longUrl.length });
-      const response = await fetch('https://my.ket.horse/api/tsdice/share', {
+      const response = await fetch('https://my.ket.horse/emoji', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
